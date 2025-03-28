1460800f09c1c1575169e47a07564b88ece74b76
Dipta Das <152770104+Dipta-17@users.noreply.github.com>
2025-03-28 14:05:00 +0000

    array_1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,15 @@
 //array
-const myArr=[0,1,2,3,4,5]
-const myHeors=['shaktiman','naagraj']
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2=new Aarray(1,2,3,4)
-console.log(myArr[1]);
+const myArr2 = new Array(1, 2, 3, 4)
+
+//console.log(myArr[1]);
+
+//Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+console.log(myArr);
