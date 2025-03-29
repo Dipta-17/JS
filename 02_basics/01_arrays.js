0b766fe531781b24b19ec3d555f6c131140ce48f
Dipta Das <152770104+Dipta-17@users.noreply.github.com>
2025-03-29 04:53:21 +0000

    array_1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,8 +8,35 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 //Array methods
 
-myArr.push(6)
-myArr.push(7)
-myArr.pop()
 
-console.log(myArr);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+//myArr.unshift(9)//[ 9, 0, 1, 2, 3,4,5]
+
+//myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.includes(4))
+// console.log(myArr.indexOf(3))
+
+const newArr=myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);// returns in string type
+// console.log(typeof newArr);
+
+
+//slice,splice
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr)
+
+const myn2=myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
