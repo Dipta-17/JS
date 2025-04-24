7cc47e4995dd92ff71823057d3f4723a121d23c7
Dipta Das <152770104+Dipta-17@users.noreply.github.com>
2025-04-24 08:07:45 +0000

    promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,45 @@
+const promiseOne=new Promise(function(resolve,reject){
+    //do an async task
+    //db calls ,cryptography ,network
+
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log('promise consumed');
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('Async task 2');
+        resolve()
+    },1000)
+}).then(function(){
+    console.log('Async task 2')
+})
+
+const promiseThree=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"Dipta",email:"dipta@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        if(!error){
+            resolve({username:"Dipta",password:"123"})
+        }else{
+            reject('Error:Somthing went wrong')
+        }
+    },1000)
+})
+
+promiseFour
