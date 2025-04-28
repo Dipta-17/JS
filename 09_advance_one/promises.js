8edf9448b4d66818089fc94f52c6a6365dca39fc
Dipta Das <152770104+Dipta-17@users.noreply.github.com>
2025-04-28 08:32:30 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -43,3 +43,61 @@ const promiseFour=new Promise(function(resolve,reject){
 })
 
 promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+
+}).finally(()=>console.log("The promise is either resolve or rejected"))
+
+
+const promiseFive=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        if (!error){
+            resolve({username:"javascript",password:"123"})
+        }else{
+            reject('ERROR:JS want wrong')
+        }
+    },1000)
+});
+
+async function  consumePromiseFive() {
+
+    try{
+        const response=await promiseFive
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function  getAllUsers() {
+//     try{
+//         const response=await fatch('https://jsonplaceholder.typicode.com/users')
+//         const data=await response.json()
+//         console.log(data);
+//     }catch(error){
+//         console.log('E:',error);
+//     }
+    
+// }
+
+// getAllUsers()
+
+fetch('https://api.github.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
+
+
+//promiseFive.all
